6024863bfe1a1a6767572e624cdb8250fe2dcc09
Thomas Martin <tmartin@ThomasMartin.local>
2020-10-01 17:03:52 +0200

    Adjust colors.vue template rendering

--- defaults/colors/colors.vue
+++ defaults/colors/colors.vue
@@ -5,22 +5,20 @@
             <div v-for="(colors, group) in datas" >
                 <h3>{{ group }}</h3>
                 <div class="sg-MainContent-list--column">
-                    <div v-for="(valCol, keyCol) in colors">
+                    <template v-for="(valCol, keyCol) in colors">
                         <div class="sg-Card" v-bind:style="{'background-color': valCol.value}">
                             <div>
                                 <span>{{keyCol}}</span><span>{{valCol.value}}</span>
                             </div>
                         </div>
                         <template v-if="valCol.declinaisons">
-                            <div v-for="(valDecli, keyDecli) in valCol.declinaisons">
-                                <div class="sg-Card" v-bind:style="{'background-color': valDecli}">
-                                    <div>
-                                        <span>{{keyCol}}-{{keyDecli}}</span><span>{{valDecli}}</span>
-                                    </div>
+                            <div v-for="(valDecli, keyDecli) in valCol.declinaisons" class="sg-Card" v-bind:style="{'background-color': valDecli}">
+                                <div>
+                                    <span>{{keyCol}}-{{keyDecli}}</span><span>{{valDecli}}</span>
                                 </div>
                             </div>
                         </template>
-                    </div>
+                    </template>
                 </div>
             </div>
         </div>
@@ -31,10 +29,10 @@
   export default {
     props: {
       datas: {
-          type: Object,
-          default: () => {
-              {}
-          }
+        type: Object,
+        default: () => {
+          {}
+        }
       }
     }
   }
