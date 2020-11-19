4b2dc4765c442970f996615c0b4d7b1d7df424d4
Thomas Martin <tmartin@ThomasMartin.local>
2020-11-19 10:42:25 +0100

    Add twig template equivalent. And add missing gradient.vue template

---
+++ defaults/colors/gradients.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="sg-MainContent-container">
+    <h2 class="sg-MainContent-title">Gradients</h2>
+    <div class="sg-DesignSystemItem">
+      <template v-for="(colors, family) in datas">
+        <div class="sg-MainContent-list--column">
+          <div class="sg-Card" v-bind:style="{height: '50px', 'background-image': 'linear-gradient(to right, ' + (colors.join(',')) + ')', 'background-size': '25% 100%'}">
+            <div>
+              <span>{{ family }}</span>
+            </div>
+          </div>
+        </div>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    datas: {
+      type: Object,
+      default: () => {
+        {
+        }
+      }
+    }
+  }
+}
+</script>
